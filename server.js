b91e8a5c8015dbdfc3bc7f59fcb9eebe1ff87d44
Alexandre Souza <alexandre.a.souza@gmail.com>
2026-05-11 12:23:56 -0300

    Fix missing and generic images

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.static(path.join(__dirname)));
 
 // Dados em memória (Mock) para rodar no Vercel sem quebrar
 const productsMock = [
-    { id: 1, name: "Goró Original", description: "0% Estimulantes, 100% Vibe. O clássico da Mansão.", price: 12.90, image_url: "https://lh3.googleusercontent.com/aida-public/AB6AXuA_JJsYigz7abbkVXKOUqM1zO5T7YgVg7kwr3pbuvJaphAAwI3-kJ0ysbpmFMKIojZdDgC1UdmUy0HZM3rrCQMJmfw029hsQIRUH-bQqJ7VgEEoMB_6wAvjnUUGVIkYCWqxNsRUJjRfWH8RJtGhBLjsBsAyW-hL4fXpxObPfhnlpfWUYZgkPX2fLOLuNLTHU7pViOMTHHSF7woY0AnkSG0K_N8zwqHfiKMl3LazyK0fHKb70iTQqoa0nhjhyeJ1_S19TV18vs64yw", category: "Energéticos" },
+    { id: 1, name: "Goró Original", description: "0% Estimulantes, 100% Vibe. O clássico da Mansão.", price: 12.90, image_url: "/goro_hero_promo.png", category: "Energéticos" },
     { id: 2, name: "Goró Zero Sugar", description: "O sabor da vitória sem açúcar e sem calorias.", price: 14.90, image_url: "/goro_zero.png", category: "Energéticos" },
-    { id: 3, name: "Monstro Juice", description: "Fórmula hardcore para treinos intensos.", price: 19.90, image_url: "https://lh3.googleusercontent.com/aida-public/AB6AXuCL42T2Win-7zZIKLB4hFJHHTPYwpSUxRIUkfoc1x2xjT8bmz472CBEgHlkjYMxve4rPsQh58cy2j1mgGD3jz5DvBXtr02zFNifiKU5-UTdO-3-6M-8b-N2tIIiJ_xbzsNUOs3ljwl4_DCgfJ-bQD_a2DLyCoyi01jfxsz1IZ8YcJtBQhNeSA339nVU788x-8xmJLNM1i5YKrKr04NutUgUUmgb6BDVgRovFZtmP_dB1Y_8NVLWRZTI8bgMZjZeDle5zPj9iGnwtQ", category: "Elite" },
-    { id: 4, name: "Gold Reserve", description: "Edição limitada com eletrólitos premium.", price: 24.90, image_url: "https://lh3.googleusercontent.com/aida-public/AB6AXuCnfDEaLPF422u0HBQbpWK-BdDCiXgoJMxnHXn2UhD5LQEzTHU77RsSPCRxMgDoF5oh_3vQbyTtD8L6VWGgEkib1dNBc4bh_F6oWCMxkJWfyiA1GxBS4xeo20snr1mSM1xjFcaPERcKnqyrFYSWz4r0UUs_j2MYbaOiN2Exk9awZ0nYavsOhFW3Tl2tyNRrTFMqWuyd9PVKnhjlodkhWJn4WcMmVplazprXzkwj563eGKiJdlmBPvkCsEFsI76NOP2cauxt-XF2FA", category: "Elite" },
+    { id: 3, name: "Monstro Juice", description: "Fórmula hardcore para treinos intensos.", price: 19.90, image_url: "/goro_vibe.png", category: "Elite" },
+    { id: 4, name: "Gold Reserve", description: "Edição limitada com eletrólitos premium.", price: 24.90, image_url: "/goro_elite.png", category: "Elite" },
     { id: 5, name: "Fella Energy", description: "A vibe da quebrada em cada gole.", price: 15.90, image_url: "/goro_fella.png", category: "Energéticos" }
 ];
 
